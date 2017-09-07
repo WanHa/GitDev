dded840a0531042575efcb9d37ff670d594334ca
WanHa <1043346131@qq.com>
2017-09-07 20:40:53 +0800

    Controller测试

---
+++ Attendance System/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Attendance_System.Controllers
+{
+    public class TestController : ApiController
+    {
+    }
+}
